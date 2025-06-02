c2c9f8cb091de25e29f32fb69aad7e29aa6898b1
jreleaserbot <jreleaser@kordamp.org>
2025-06-02 19:12:40 +0000

    spotless-cli v0.1.1

--- Formula/spotless-cli.rb
+++ Formula/spotless-cli.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.18.0 at 2025-05-13T10:49:41.653433121Z
+# Generated with JReleaser 1.18.0 at 2025-06-02T19:12:37.7666696Z
 
 class SpotlessCli < Formula
   desc "Keep your code Spotless from the command line"
   homepage "https://github.com/diffplug/spotless-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "Apache-2.0"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.0/spotless-0.1.0-linux-aarch_64.zip"
-    sha256 "e0b50da040b7ee8b77265550d15af651b8ec6a6d1a3f28e544f6d6787c0772ac"
+    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.1/spotless-0.1.1-linux-aarch_64.zip"
+    sha256 "964a0788e98ee2dd93e8352a228d5b42a6723a3a250ab21642b8f6a97182fb33"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.0/spotless-0.1.0-linux-x86_64.zip"
-    sha256 "5de3f2402539e42273bace5e989d0c3749f9fcd8f89e9de83d996ae58b7fed96"
+    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.1/spotless-0.1.1-linux-x86_64.zip"
+    sha256 "f45b58da075c4e32b396fa509422f9ff422b5f6e211f56b92b2d938df6e63be4"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.0/spotless-0.1.0-osx-aarch_64.zip"
-    sha256 "152fc4118b7b63224611e6cbc32f1d36e628d47771b2993bf31dea000fbf631c"
+    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.1/spotless-0.1.1-osx-aarch_64.zip"
+    sha256 "6dec4aea6953807c77b514487afd490ed7e933213c305a70c9476d7d7d5b27d7"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.0/spotless-0.1.0-osx-x86_64.zip"
-    sha256 "12bb2653b5df4a5655a62c64720429697a0f376f7fabb677ec4800de7abf233b"
+    url "https://github.com/diffplug/spotless-cli/releases/download/v0.1.1/spotless-0.1.1-osx-x86_64.zip"
+    sha256 "d7b1711f29c16520ffe107a0f1b7cbb76ae037e86a1957c420956aedf807a8c5"
   end
 
 
@@ -31,6 +31,6 @@ class SpotlessCli < Formula
 
   test do
     output = shell_output("#{bin}/spotless --version")
-    assert_match "0.1.0", output
+    assert_match "0.1.1", output
   end
 end
